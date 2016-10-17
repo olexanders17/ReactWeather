8ac1ce3c7751c8edac823d33d56c9d428a099c28
olexanders17 <olexanders17@gmail.com>
2016-10-17 19:54:28 +0300

    update server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     entry: './app/app.jsx',
     output: {
         path: __dirname,
-        filename: './app/bundle.js'
+        filename: './public/bundle.js'
     },
     resolve: {
         root:__dirname,
